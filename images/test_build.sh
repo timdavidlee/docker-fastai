67c4cdf16e702097f7c53d44be4feebff750b8e6
timdavidlee <tim.david.lee@gmail.com>
2019-03-02 18:26:05 -0800

    updating build script

---
+++ images/test_build.sh
@@ -0,0 +1,6 @@
+docker build --rm \
+             -f fastai-template.Dockerfile \
+             --build-arg CUDA='10.0' \
+             --build-arg UBUNTU_VERSION='18.04' \
+             --build-arg PYTHON_VERSION='3.6' \
+             --build-arg CUDNN='7'
