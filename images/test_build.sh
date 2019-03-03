eae00512d922dded70b2d26b5643340735c64700
timdavidlee <tim.david.lee@gmail.com>
2019-03-02 18:28:18 -0800

    updating

--- images/test_build.sh
+++ images/test_build.sh
@@ -1,7 +1,7 @@
 docker build --rm \
-             -f fastai-template.Dockerfile \
+             -f ./fastai-template.Dockerfile \
              --build-arg CUDA='10.0' \
              --build-arg UBUNTU_VERSION='18.04' \
              --build-arg PYTHON_VERSION='3.6' \
              --build-arg CUDNN='7'
-             chaffix/fastai:test_build
+             -t chaffix/fastai:test_build
