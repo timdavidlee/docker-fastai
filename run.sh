80aca9f0634b58fefe4ce1bad517407a31fc4343
timdavidlee <tim.david.lee@gmail.com>
2019-03-05 22:12:59 -0800

    ditto

--- run.sh
+++ run.sh
@@ -1,4 +1,17 @@
-docker run --rm \
-           --runtime=NVIDIA \
-           --name fastai_test_container \
+mkdir persist_data
+PASSWORD=$1
+
+if [[ "$PASSWORD" = ""]]; then
+  echo "please provide a notebook password after the run command"
+  echo "./run.sh <MYPASSWORD>"
+  exit 1
+fi
+
+docker run --rm -it -d \
+           --runtime=nvidia \
+           --shm-size=1g \
+           -p 8888 \
+           -v persist_data/:/persist_data/ \
+           --name fastai_gpu_jup_container \
+           -e PASSWORD=$PASSWORD
            chaffix/fastai:stable
